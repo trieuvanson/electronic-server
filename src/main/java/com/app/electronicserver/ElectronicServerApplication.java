9daddf8d7af03b2736222c3cd1fe8a515f905238
trieuvanson <Vodanhserver@gmail.com>
2021-10-02 20:42:57 +0700

    Initial commit

---
+++ src/main/java/com/app/electronicserver/ElectronicServerApplication.java
@@ -0,0 +1,13 @@
+package com.app.electronicserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/electronicserver/ElectronicServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.electronicserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
