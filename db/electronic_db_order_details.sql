6a67b67e992d90436a8e1aeade584b97eadc734e
Tân Dai <hoangdai12022001@gmail.com>
2021-11-26 20:11:04 +0700

    db

---
+++ db/electronic_db_order_details.sql
@@ -0,0 +1,8 @@
+INSERT INTO `electronic-db`.order_details (id, price, quantity, order_id, product_id) VALUES (1, 11200000, 1, 2, 11);
+INSERT INTO `electronic-db`.order_details (id, price, quantity, order_id, product_id) VALUES (2, 11200000, 1, 2, 11);
+INSERT INTO `electronic-db`.order_details (id, price, quantity, order_id, product_id) VALUES (3, 11200000, 1, 2, 10);
+INSERT INTO `electronic-db`.order_details (id, price, quantity, order_id, product_id) VALUES (4, 11200000, 1, 2, 12);
+INSERT INTO `electronic-db`.order_details (id, price, quantity, order_id, product_id) VALUES (5, 11200000, 1, 2, 12);
+INSERT INTO `electronic-db`.order_details (id, price, quantity, order_id, product_id) VALUES (6, 11200000, 1, 2, 12);
+INSERT INTO `electronic-db`.order_details (id, price, quantity, order_id, product_id) VALUES (7, 11200000, 1, 2, 9);
+INSERT INTO `electronic-db`.order_details (id, price, quantity, order_id, product_id) VALUES (8, 11200000, 1, 2, 9);
